77d3afa583f27b8e992c4d5f49179d27b5ccb39e
Your Name <marlonyurimi@gmail.com>
2022-09-23 19:31:35 -0300

    first

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Header = styled.header`
+
+width: 100vw;
+height: 30vh;
+background-color:pink;
+display: flex;
+justify-content: center;
+align-items: center;
+border:dashed 3px red;
+`
+
+export default function App() {
+  return (
+    <Header>
+
+      <h1>Oi</h1>
+
+    </Header>
+  )
+}
